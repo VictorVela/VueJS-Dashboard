d13a318171c22adee19ed1a9974e225873179a89
Victor Vela <victorgvela@hotmail.com>
2020-07-20 08:10:53 -0300

    fist commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Dashboard />
+    <div id="box-b">
+      <DashBoxTotal />
+      <DashBoxSafe/>
+      <DashBoxAlert />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Dashboard from "./components/Dashboard.vue";
+import DashBoxTotal from "./components/DashBox_total.vue";
+import DashBoxAlert from "./components/DashBox_alert.vue";
+import DashBoxSafe from "./components/DashBox_safe.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Dashboard,
+    DashBoxTotal,
+    DashBoxAlert,
+    DashBoxSafe
   }
-}
+};
 </script>
 
 <style>
+body {
+  background: rgb(240, 240, 240);
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#box-b {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-between;
+  margin: 150px;
 }
 </style>
